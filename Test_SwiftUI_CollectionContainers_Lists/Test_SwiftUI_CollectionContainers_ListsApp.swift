bb42c7aae064da37430c72f7bc1452c4e66d8f2a
JTrojanowicz <jaroslaw.trojanowicz@gmail.com>
2022-07-18 08:28:01 +0200

    Initial Commit

---
+++ Test_SwiftUI_CollectionContainers_Lists/Test_SwiftUI_CollectionContainers_ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_SwiftUI_CollectionContainers_ListsApp.swift
+//  Test_SwiftUI_CollectionContainers_Lists
+//
+//  Created by Jaroslaw Trojanowicz on 18/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_SwiftUI_CollectionContainers_ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
